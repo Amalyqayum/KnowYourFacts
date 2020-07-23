ec7e5fd04a4ef341994f0f0eeca79d356f1a3644
18016222 <amalyqayum@gmail.com>
2020-07-23 12:12:44 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/knowyourfacts/Frag3.java
@@ -0,0 +1,50 @@
+package com.myapplicationdev.android.knowyourfacts;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.Random;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Frag3#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Frag3 extends Fragment {
+    Button btnColor;
+    LinearLayout bg;
+
+    public Frag3() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_3, container, false);
+        btnColor = view.findViewById(R.id.btnColor2);
+        bg = view.findViewById(R.id.bg2);
+
+        btnColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                bg.setBackgroundColor(color);
+            }
+        });
+        return view;
+    }
+}
+
